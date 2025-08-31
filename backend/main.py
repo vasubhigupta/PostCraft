7b7608a5fd88fbf7344a817ccd8bd949a5c6bcab
Vasubhi Gupta <vasubhigupta@gmail.com>
2025-08-31 21:36:24 +0530

    Adding Frontend Deployment Address

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://post-craft-sigma.vercel.app/, http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
